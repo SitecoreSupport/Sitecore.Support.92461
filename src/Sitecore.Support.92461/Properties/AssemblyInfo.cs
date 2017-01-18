b851d546f81bfc52b9cf57c0271e5a7d6f42e34e
Anton Kechashin <atk@sitecore.net>
2017-01-18 15:11:05 +0200

    Initial version

---
+++ src/Sitecore.Support.92461/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.92461")]
+[assembly: AssemblyProduct("Sitecore.Support.92461")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.92461/Shell/Applications/ContentEditor/File.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.Support.Shell.Applications.ContentEditor
+{
+    using Sitecore.Web.UI.Sheer;
+
+    public class File : Sitecore.Shell.Applications.ContentEditor.File
+    {
+        protected override void DoChange(Message message)
+        {
+            #region Modified code
+            if (this.Value == string.Empty)
+            {
+                this.XmlValue = new Sitecore.Shell.Applications.ContentEditor.XmlValue(string.Empty, "file"); // if the value of the field is changed to empty, clear the raw value
+            }
+            else
+            {
+                base.DoChange(message);
+            } 
+            #endregion
+        }
+
+        private Sitecore.Shell.Applications.ContentEditor.XmlValue XmlValue
+        {
+            get
+            {
+                Sitecore.Shell.Applications.ContentEditor.XmlValue viewStateProperty = base.GetViewStateProperty("XmlValue", null) as Sitecore.Shell.Applications.ContentEditor.XmlValue;
+                if (viewStateProperty == null)
+                {
+                    viewStateProperty = new Sitecore.Shell.Applications.ContentEditor.XmlValue(string.Empty, "file");
+                    this.XmlValue = viewStateProperty;
+                }
+                return viewStateProperty;
+            }
+            set
+            {
+                base.SetViewStateProperty("XmlValue", value, null);
+            }
+        }
+    }
+}
